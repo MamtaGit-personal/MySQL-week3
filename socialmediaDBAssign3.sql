7639daec2215b4bdd6c26fcc1714fbc695f4308b
Mamta Nath <mamtanathster@gmail.com>
2021-05-24 13:40:29 -0700

    tweaked posts and comments tables

--- socialmediaDBAssign3.sql
+++ socialmediaDBAssign3.sql
@@ -9,10 +9,10 @@ drop table if exists users;
 CREATE TABLE users(
 	id int(11) not null auto_increment,
 	username varchar(30) not null,
-	password varchar(30) not null,
-	email varchar(40) not null,
+	password varchar(15) not null,
+	email varchar(50) not null,
 	phone_number varchar(16) not null,
-	first_name varchar(20) not null,
+	first_name varchar(20),
 	last_name varchar(20),
 	primary key(id)
 
@@ -22,20 +22,22 @@ CREATE TABLE users(
 CREATE TABLE posts(
 	id int(11) not null auto_increment,
 	user_id int(11) not null,
-	post Text not null,
-	creation_time DATETIME default current_timestamp,
-	primary key(id),
+	post_title varchar(100) not null,
+    post_body Text not null,
+	create_time DATETIME default now(),
+    primary key(id),
 	foreign key (user_id) references users(id)
 	
 );
 
 
 CREATE TABLE comments(
-	user_id int(11) not null,
+	id int(11) not null auto_increment,
+    user_id int(11) not null,
 	post_id int(11) not null,
-	comment Text not null,
-	creation_time DATETIME default current_timestamp,
-	primary key(user_id, post_id),
+    comment_body Text not null,
+	create_time DATETIME default now(),
+    primary key(user_id, post_id),
 	foreign key (user_id) references users(id),
 	foreign key (post_id) references posts(id)
 );
